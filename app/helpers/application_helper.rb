fada851d3a0167ab676197bdb650359f15be8511
Jonathan Knight <jonathan@jrlknight.com>
2012-03-22 11:47:17 +0000

    Updating Actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,4 +35,10 @@ module ApplicationHelper
     end
   end
   
+  def present(object, klass = nil)
+    klass ||= "{object.class}Presenter".constantize
+    presenter = klass.new(object, self)
+    yield presenter if block_given?
+    presenter
+  end
 end
